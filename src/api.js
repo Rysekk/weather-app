0b52a783d5ce2e4bdbcf2da8c3fab0a9288f12ce
Rysekk <m.aubry130@gmail.com>
2023-07-03 12:47:41 +0200

    add project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e689380b54msh28b18767140ae2dp10dbb8jsn52086760b590',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "e6041da0669d87e9711a969912ac292d"
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,38 @@
+import "./current-weather.css";
+
+const CurrentWeather = ({ data }) => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
+        </div>
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`}></img>
+      </div>
+      <div className="bottom">
+        <p className="temperature">{data.temp}</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed}</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidité</span>
+            <span className="parameter-value">{data.main.humidity}</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pression</span>
+            <span className="parameter-value">{data.main.pressure}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
